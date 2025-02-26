73b606006d3e4c9186a840dc90de230a1618410a
Yahya_Syauqoni25 <psyauqoni@gmail.com>
2025-02-26 09:14:11 +0700

    commit yas

---
+++ README.md
@@ -0,0 +1,5 @@
+# ----------------------------------------------------------------------------------------
+# Hola Mi Amigos!.
+## ini adalah Aplikasi Al-Qur'an yang sudah saya buat, setelah banyak mengikuti tutorial flutter, akhirnya jadi juga.
+### ( ini masih sementara dan ongoing ).
+# ----------------------------------------------------------------------------------------
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:the_holy_quran/screens/splashscreen_disini.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
